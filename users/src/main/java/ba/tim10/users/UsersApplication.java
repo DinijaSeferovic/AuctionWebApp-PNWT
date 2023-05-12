38df50cdf482a69d4d0063b135c4c8a135121288
DinijaSeferovic <dseferovic2@etf.unsa.ba>
2023-05-12 16:13:03 +0200

    fix dependencies

--- users/src/main/java/ba/tim10/users/UsersApplication.java
+++ users/src/main/java/ba/tim10/users/UsersApplication.java
@@ -4,16 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableCircuitBreaker
 public class UsersApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(UsersApplication.class);
